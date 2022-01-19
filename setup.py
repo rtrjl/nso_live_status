b90a19e976e77a8cf0ae22a9010e1bc220eb1237
Rodolphe TRUJILLO -X (rodtruji - INFRASPEC SAS at Cisco) <rodtruji@cisco.com>
2022-01-19 14:57:16 +0100

    setup.py

--- setup.py
+++ setup.py
@@ -14,16 +14,16 @@ setup(
     description="Retrieve cli result and parse it using NSO live status and PyATS parsers",
     long_description=README,
     long_description_content_type="text/markdown",
-    url="https://github.com/rtrjl/pyats_parser",
+    url="https://github.com/rtrjl/nso_live_status",
     author="Rodolphe Trujillo",
     author_email="rodtruji@cisco.com",
     license="Cisco Sample Code License, Version 1.1",
     classifiers=[
         "Topic :: System :: Networking",
     ],
-    packages=["pyats_parser"],
+    packages=["nso_live_status"],
     include_package_data=True,
-    install_requires=["pyats", "genie"],
+    install_requires=["pyats_parser"],
     requires_python='>=3.6.0'
 
 )
